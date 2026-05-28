32ac419712e1bbc41720c888fb1512a5ab385f30
Vitor <195686982+vdss-x00@users.noreply.github.com>
2026-05-27 21:48:54 -0300

    Imports iniciais

--- src/paginas/AvaliaProduto.jsx
+++ src/paginas/AvaliaProduto.jsx
@@ -1,3 +1,16 @@
+import HighBay from "../assets/high_bay.png";
+import Refletor from "../assets/refletor_ultra.png";
+import EX from "../assets/luminaria_ex.png";
+import Industriais from "../assets/luminaria_industrial.png";
+import IlumPubli from "../assets/luminaria_publica.png";
+import Lineares from "../assets/linear_ip69k.png";
+import highBayModular from "../assets/high_bay_modular.png";
+import linearIp66 from "../assets/linear_ip66.png";
+import luminariaDeco from "../assets/luminaria_deco.png";
+import luminariaFrigorifico from "../assets/luminaria_frigorifico.png";
+
+
+
 function AvaliaProduto() {
     return(
         {/* Conteúdo da página de avaliação do produto */}
