74f46db4c969f5df695e50d88e0d14a040c3dd4e
Vitor Santos <195686982+vdss-x00@users.noreply.github.com>
2026-05-29 17:18:59 -0300

    Remover cards desnecessários de garantia e proteção

--- src/paginas/AvaliaProduto.jsx
+++ src/paginas/AvaliaProduto.jsx
@@ -102,16 +102,6 @@ function AvaliaProduto() {
                 <h2 className="text-xl font-semibold text-zinc-600 dark:text-zinc-300">
                   {product.modelo}
                 </h2>
-
-                <div className="mt-4 flex flex-wrap items-center justify-center gap-3">
-                  <div className=" inline-block p-1 bg-[#d9d9d9] text-zinc-900">
-                    <p>{product.garantia}</p>
-                  </div>
-
-                  <div className=" inline-block p-1 bg-zinc-100 dark:bg-[#303030]">
-                    <p>Proteção IP69K</p>
-                  </div>
-                </div>
               </div>
 
               <div className="w-full rounded-2xl bg-zinc-100 p-4 text-left dark:bg-[#303030]">
